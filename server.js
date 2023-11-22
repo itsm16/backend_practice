28a210ceb59aa5a590afdbc9bf74d0e57438d7c1
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-11-23 02:08:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose';
 import morgan from 'morgan'
 
 const app = express();
@@ -7,17 +8,46 @@ app.set('view engine','ejs')
 app.use(morgan('common'))
 app.use(express.urlencoded({extended:true}))
 
+mongoose.connect('mongodb://127.0.0.1:27017',({
+    dbName:'backend'
+}))
+.then(()=> console.log('connected'))
+.catch((e) => console.log(e))
+
+const infoSchema = new mongoose.Schema({
+    email:String,
+    password:String
+}
+)
+
+const info = mongoose.model('info',infoSchema)
+
+const userInfo = [];
+
 app.get('/',(req,res)=>{
     res.send('hey')
 });
 
-app.get('/pg1',(req,res)=>{
-    res.render('page1')
+app.get('/add',(req,res)=>{
+    info.create({email:'k',password:'k'})
+    .then(()=>{
+        res.send('Sent')
+    })
+})
+
+app.get('/getInfo',(req,res)=>{
+    res.render('getInfo')
+})
+
+app.get('/info',(req,res)=>{
+    res.send(userInfo)
 })
 
 app.post('/pg1',(req,res)=>{
-    res.redirect('/')
     console.log(req.body)
+    userInfo.push(req.body)
+    info.create({email:req.body.email,password:req.body.password})
+    .then(()=>res.redirect('/info'))    
 })
 
 app.listen(3000,()=>{
