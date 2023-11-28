4fcfc1fd1808f9d3ccc28723e2fcb74d2aed6cc2
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-11-28 18:13:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const app = express();
 
 app.set('view engine','ejs')
 app.use(morgan('common'))
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+//app.use(express.static('public')) // for css
 
 mongoose.connect('mongodb://127.0.0.1:27017',({
     dbName:'Backend'
