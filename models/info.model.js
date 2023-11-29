8c9da155240c910e65dd4996316e3bffbd3680ea
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-11-29 16:02:09 +0530

    Create info.model.js

---
+++ models/info.model.js
@@ -0,0 +1,22 @@
+import { mongoose } from "mongoose";
+
+mongoose.connect('mongodb://127.0.0.1:27017',({
+    dbName:'Backend'
+}))
+.then(()=> console.log('Database Connected'))
+.catch((e) => console.log(e))
+
+const infoSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+//export const info = mongoose.model('info',infoSchema); // info is used as collection name , infos collection in database gets created
+// since export is used above ,to import info in server.js....it should be inside braces - {info}
+
+
+// to use default it can written as ----
+//the export below doesn't require {} while importing
+const info = mongoose.model('info',infoSchema);
+export default info;
