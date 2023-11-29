bf3e155b6d4377610dbfc85c406bdaeea7a2d07a
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-11-29 16:09:39 +0530

    Update info.model.js

--- models/info.model.js
+++ models/info.model.js
@@ -7,7 +7,10 @@ mongoose.connect('mongodb://127.0.0.1:27017',({
 .catch((e) => console.log(e))
 
 const infoSchema = new mongoose.Schema({
-    email:String,
+    email:{
+        type:String,
+        required:true
+    },
     password:String
 });
 
