d0b5918a60a79dcf577e709d9607d86364b1e706
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-12-02 18:06:20 +0530

    Update info.model.js

--- models/info.model.js
+++ models/info.model.js
@@ -1,36 +1,13 @@
 import { mongoose } from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017',({
-    dbName:'Backend'
-}))
-.then(()=> console.log('Database Connected'))
-.catch((e) => console.log(e))
-
 const infoSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true
     },
-    password:String,
-    
-    /*
-     field_name:{
-         type:mongoose.Schema.Types.ObjectId,    //this is a method used to take reference from another schema
-         ref:'model_name'    // ref should be used to get reference , name of schema to take reference is added here
-     }
-     
-    field_name:{
-         type:[]    // sometimes types are an array but js using array doesn't solve it ...such as an array for orderItems , defining quantity etc. ↓↓↓
-                    // In such cases ANOTHER SCHEMA for the items inside array is created/used
-            
-         enum:''    // in few cases when type is string , and a preDefined set of strings are need to be provided ...we use
-                     //in such cases enum is used which provides choices to use for the string
-
-         default:''  // default provides a default string that is used   
-     }
-    */
-    
-},{timestamps:true}); //timestamps - gives createdAt , updatedAt
+    password:String
+       
+},{timestamps:true});
 
 
 //export const info = mongoose.model('info',infoSchema); // info is used as collection name , infos collection in database gets created
