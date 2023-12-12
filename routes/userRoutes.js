ac9b694510f96ff5862d9eba7e6f157f3d9efe07
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-12-13 00:28:36 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,10 +37,108 @@ Router.post("/register",async (req,res)=>{
             expires: new Date(Date.now() + 60 * 1000), 
         });
     
+        //sign method takes a secret
+        //user's id(written as user._id) is provided with secret to sign method
+        //token is passed as value in res.cookie method
+
         // Redirect to the authenticated route
         res.redirect("/authIn");
     
         
+})
+
+
+// Login User
+
+const isAuthenticated = async (req,res,next)=>{
+    const {authToken} = req.cookies;
+    if (authToken) {
+        const decoded = jwt.verify(authToken,"oneTwo");
+        req.user = await info.findById(decoded._id)
+        next();
+    } else {
+        res.redirect("/authLogin")
+    }
+}
+
+Router.get('/authLogin',(req,res)=>{
+    //created in isAuthenticated
+   // res.render('authSecret',{email : req.user.email})
+   res.render('authLogin')
+   
+})
+
+Router.post("/authLogin", async (req, res) => {
+    const { email, password } = req.body;
+
+    // Check if the user exists
+    let user = await info.findOne({ email });
+
+    if (!user) {
+        return res.redirect('/register'); // or handle the error accordingly
+    }
+
+    // Compare passwords only if the user exists
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.redirect("/authLogin");
+
+    }
+        const token = jwt.sign({ _id: user._id }, "oneTwo");
+
+        res.cookie("authToken", token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 60 * 1000),
+        });
+
+        return res.redirect("/authIn");
+    
+});
+
+/*
+app.post('/authLogin', async (req, res) => {
+    const { email, password } = req.body;
+    console.table(req.body);
+
+    // Find the user by email
+    const user = await info.findOne({ email });
+
+    // Check if the user exists
+    if (!user) {
+        return res.redirect('/register'); // or handle the error accordingly
+    }
+
+    // Compare the hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    // If passwords match, generate JWT token and set cookie
+    if (passwordMatch) {
+        const token = jwt.sign({ _id: user._id }, "oneTwo");
+        console.table([password,user.password])
+        res.cookie('authToken', token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 30 * 1000) 
+        });
+        return res.redirect('/authIn');
+    } else {
+        // Handle incorrect password
+        return res.redirect('/authLogin'); // or handle the error accordingly
+    }
+    
+});
+*/
+
+Router.get('/authIn',isAuthenticated,(req,res)=>{
+    res.render('authSecret',{email:req.user.email})
+})
+
+Router.get("/authout",(req,res)=>{
+    res.cookie("authToken", null, {
+        httpOnly: true,
+        expires: new Date(Date.now())
+      });
+    res.redirect("/authLogin");
 })
 
 export default Router;
