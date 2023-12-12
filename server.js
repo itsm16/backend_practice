c1018e9a4d01de0ec6996ea72ac5419058d4b3ab
itsm16 <111368673+itsm16@users.noreply.github.com>
2023-12-13 00:09:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import morgan, { token } from 'morgan'
+import express, { Router } from 'express'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
 dotenv.config();
 import cookieParser from 'cookie-parser';
@@ -7,6 +7,7 @@ import info from './models/info.model.js'
 import connectDb from './db/db.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import userRouter from './routes/userRoutes.js'
 
 // Database
 connectDb();
@@ -18,6 +19,7 @@ app.set('view engine','ejs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(userRouter);
 
 // custom middlewares 
 app.use((req,res,next)=>{
@@ -43,7 +45,7 @@ const isAuthenticated = async (req,res,next)=>{
         req.user = await info.findById(decoded._id)
         next();
     } else {
-        res.render('authLogin')
+        res.redirect("/authLogin")
     }
 }
 
@@ -53,63 +55,105 @@ app.get('/',(req,res)=>{
     res.send('hey');
 });
 
-
-// Register User
-
-app.get("/register",(req,res)=>{
-    res.render("register")
+app.get("/api/get",(req,res)=>{
+    res.send(
+        "Route : /api/get"
+    )
 })
 
-app.post("/register",async (req,res)=>{
-    const {email,password} = req.body;
-    const hashedPassword = await bcrypt.hash(password,10)
-    const existingUser = await info.findOne({ email });
+// Register User - defined in routes
 
-    if (existingUser) {
-        return res.redirect("/authLogin");
-    } else {
-        await info.create({email,password : hashedPassword})
-        res.redirect("/info")
-    }
+// Login User
+
+app.get('/authLogin',(req,res)=>{
+     //created in isAuthenticated
+    // res.render('authSecret',{email : req.user.email})
+    res.render('authLogin')
     
 })
 
-app.get('/authLogin',isAuthenticated,(req,res)=>{
-    console.log(req.user) //created in isAuthenticated
-    res.render('authSecret',{email : req.user.email})
+
+//sign method takes a secret
+//     //user's id(written as user._id) is provided with secret to sign method
+//     //token is passed as value in res.cookie method
+//     console.log(token);
+
+
+// app.post('/authLogin', async (req, res) => {
+//     const { email, password } = req.body;
+//     console.table(req.body);
+
+//     // Find the user by email
+//     const user = await info.findOne({ email });
+
+//     // Check if the user exists
+//     if (!user) {
+//         return res.redirect('/register'); // or handle the error accordingly
+//     }
+
+//     // Compare the hashed password
+//     const passwordMatch = await bcrypt.compare(password, user.password);
+
+//     // If passwords match, generate JWT token and set cookie
+//     if (passwordMatch) {
+//         const token = jwt.sign({ _id: user._id }, "oneTwo");
+//         console.table([password,user.password])
+//         res.cookie('authToken', token, {
+//             httpOnly: true,
+//             expires: new Date(Date.now() + 30 * 60 * 1000) // set expiration to 30 minutes
+//         });
+//         return res.redirect('/authIn');
+//     } else {
+//         // Handle incorrect password
+//         return res.redirect('/authLogin'); // or handle the error accordingly
+//     }
+    
+// });
+
+app.post("/authLogin", async (req, res) => {
+    const { email, password } = req.body;
+
+    // Check if the user exists
+    let user = await info.findOne({ email });
+
+    if (!user) {
+        return res.redirect('/register'); // or handle the error accordingly
+    }
+
+    // Compare passwords only if the user exists
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.redirect("/authLogin");
+
+    }
+        const token = jwt.sign({ _id: user._id }, "oneTwo");
+
+        res.cookie("authToken", token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 60 * 1000),
+        });
+
+        return res.redirect("/authIn");
+    
+});
+
+
+
+app.get('/authIn',isAuthenticated,(req,res)=>{
+    res.render('authSecret',{email:req.user.email})
 })
 
-app.post('/authLogin',async (req,res)=>{
-    const {email,password} = req.body;
-    console.table(req.body);
-    //const user = await info.create({email,password});
-    const user = await info.findOne({email,password})
-
-     if (user==req.user) {
-        const token = jwt.sign({_id : user._id},"oneTwo")
-        res.cookie('authToken',token,{   
-            httpOnly:true,
-            expires: new Date(Date.now()+30*1000)
-        })
-        res.redirect('/authLogin')
-     } else {
-        res.redirect("/register")
-     }
-
-    //sign method takes a secret
-    //user's id(written as user._id) is provided with secret to sign method
-    //token is passed as value in res.cookie method
-    console.log(token);
-})
-
-app.get('/authout',(req,res)=>{
-    res.cookie('authToken',null,{
-        httpOnly:true,
+app.get("/authout",(req,res)=>{
+    res.cookie("authToken", null, {
+        httpOnly: true,
         expires: new Date(Date.now())
-    })
-    res.redirect('/authLogin')
+      });
+    res.redirect("/authLogin");
 })
 
+
+// Show Database Info
 app.get('/getInfo',(req,res)=>{
     res.render('getInfo');
 })
@@ -138,15 +182,6 @@ app.get('/login',isAuth,(req,res)=>{
     res.render('secret')    //action="/logout" action attribute in secret.ejs form is set to /logout
 })
 
-app.get('/logout',(req,res)=>{
-    res.cookie('token',null,{
-        httpOnly:true,
-        expires: new Date(Date.now())
-    })
-    
-    res.redirect('/login');
-})
-
 app.post('/login',(req,res)=>{
     res.cookie('token','iamIn',{
         httpOnly:true,
@@ -154,6 +189,15 @@ app.post('/login',(req,res)=>{
     });
     res.redirect('/login');
 })
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('token', {
+        httpOnly: true,
+        expires: new Date(0) // Set the expiration date to a past date
+    });
+
+    res.redirect('/authLogin');
+});
 
 app.listen(PORT,()=>{
     console.log(`Running on ${PORT}`);
